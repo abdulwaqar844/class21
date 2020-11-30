344833e6bc2ea91445857cf0e1a17c52a2da40b7
Abdul Waqar <abdulwaqar844@gmaill.com>
2020-12-01 01:48:45 +0500

    Mutation Functionality Added

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
 const { ApolloServer, gql } = require('apollo-server');
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+const students = [
+  {
+    "id": 1,
+    "name": "Abdul Waqar",
+    "Program": "MCS",
+    "smester": "3rd"
+  },
+  {
+    "id": 2,
+    "name": "Fayaz Ahmad",
+    "Program": "MCS",
+    "smester": "3rd"
+  },
+  {
+    "id": 3,
+    "name": "Usman Fani",
+    "Program": "BS IT",
+    "smester": "3rd"
+  }
+];
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Student {
+    id:Int
     name: String
     Program: String
     smester:String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
+  
   type Query {
     Students: [Student]
   }
-`;
-const students = [
-    {
-        "name": "Abdul Waqar",
-        "Program": "MCS",
-        "smester":"3rd"
-      },
-      {
-          "name": "Fayaz Ahmad",
-        "Program": "MCS",
-        "smester":"3rd"
-      }, {
-        "name": "Usman Fani",
-      "Program": "BS IT",
-      "smester":"3rd"
-    }
-  ];
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
+  input InputStudent {
+    id:Int
+    name: String
+    Program: String
+    smester:String
+  },
+  type Mutation {    
+    addStudent(input:InputStudent): Student
+  }`;
 const resolvers = {
-    Query: {
-      Students: () => students,
-    },
-  };
-    // The ApolloServer constructor requires two parameters: your schema
+  Query: {
+    Students: () => students,
+  },
+  Mutation: {
+    addStudent: (e, {input} ) => {
+
+      students.push(
+        { id:input.id,
+        name:input.name,
+        Program:input.Program,
+        smester:input.smester}
+       
+      )
+      return (
+     input
+      )
+    }
+  }
+};
+// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
 
